f170ecaa00eaacd5c7b495c80d40ed2ac36efe5d
ingkoon <ing97220@naver.com>
2023-08-04 17:24:05 +0900

    Add: domain 생성

---
+++ backend/src/main/java/com/eeaao/jukebox/common/entity/BaseEntity.java
@@ -0,0 +1,30 @@
+package com.eeaao.jukebox.common.entity;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+
+@Getter
+@SuperBuilder
+@NoArgsConstructor
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt = LocalDateTime.now();
+}
---
+++ backend/src/main/java/com/eeaao/jukebox/member/entity/Member.java
@@ -0,0 +1,43 @@
+package com.eeaao.jukebox.member.entity;
+
+import com.eeaao.jukebox.common.entity.BaseEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "MEMBER")
+public class Member extends BaseEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String account;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Builder
+    public Member(String account, String password, String nickname) {
+        this.account = account;
+        this.password = password;
+        this.nickname = nickname;
+    }
+
+
+    public void updateNickName(String nickname){
+        this.nickname = nickname;
+    }
+
+    public void updatePassword(String password){
+        this.password = password;
+    }
+
+}
