9e872c5d3f7627052c506ddf2a7901dfd22ce819
ingkoon <dlswotmd@gmail.com>
2023-08-21 23:25:50 +0900

    Add: health check 스크립트 수정

--- backend/container_check.sh
+++ backend/container_check.sh
@@ -1,14 +1,14 @@
 #/bin/sh
 
-SERVER_CONTAINER_ID=`docker ps -aq --filter 'tag=server'`
+SERVER_CONTAINER_ID=`docker ps -aq --filter 'name=server'`
 
 if [ -n "$SERVER_CONTAINER_ID" ];
   then
     echo "server container exist"
     docker stop $SERVER_CONTAINER_ID
     docker rm $SERVER_CONTAINER_ID
-    docker run -d -p 8081:8081 --name server eeaao:server
+    docker run -d -p 8081:8081 --name server server:eeaao
   else
     echo "node container not exist"
-    docker run -d -p 8081:8081 --name server eeaao:server
+    docker run -d -p 8081:8081 --name server server:eeaao
 fi
